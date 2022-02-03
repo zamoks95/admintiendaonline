6e2def6149882bdbabb5dc4b09147af44512287c
Sergi Zamora Montesinos <sergi.zamora.95@gmail.com>
2022-02-03 20:05:39 +0100

    strapi

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a18423801a51a2f29cfc1bb418d86cff'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5fc4e6b8-77ba-4085-bf27-1badfdd64160'
+};
